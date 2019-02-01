1dd46d23da004ce43b549443e61d4bb0ae9048d0
victorvictorvk <lavialactea89@hotmial.com>
2019-02-01 10:45:33 +0100

    Subida de la clase NODO

---
+++ Nodo.java
@@ -0,0 +1,16 @@
+package colaDinamica;
+
+public class Nodo {
+
+	Object info;
+	Nodo enlace;
+	
+	
+	public Nodo(Object info, Nodo enlace)
+	{
+		this.info = info;
+		this.enlace = enlace;
+	}
+	
+	
+}
