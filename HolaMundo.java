20ff949743e492b20d660114a4bbd7e5876524ca
Victor <lavialactea89@hotmail.com>
2019-02-07 20:52:42 +0100

    practica 3

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,7 @@
 ﻿public class Hello {
   public static void main(String[] args) {
     System.out.println("Bonjour monde!");
+    System.out.println("Yo también hago cambios en el mismo fichero");
 //ves esto sandra?
 // ¡sí lo veo!
   }
